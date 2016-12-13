d43b88ef74f2b426adb03028ea32b447e5f2bdeb
root <root@iZwz97ykmwwdhktkd90athZ.(none)>
2016-12-14 00:14:44 +0800

    a

--- protected/config/database.php
+++ protected/config/database.php
@@ -8,6 +8,6 @@ return array(
 	'connectionString' => 'mysql:host=localhost;dbname=caijia',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => 'root',
 	'charset' => 'utf8',
 );
