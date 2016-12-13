89d295c3df16df1e97cc3c82be4be71a138de45d
liuzihui531 <289747370@qq.com>
2016-12-14 00:52:25 +0800

    dd

--- protected/modules/front/views/project/place.php
+++ protected/modules/front/views/project/place.php
@@ -2,7 +2,8 @@
 	<?php if ($placeModel): ?>
 	<ul class="address">
 		<?php foreach ($placeModel as $key => $val): ?>
-			<li><a href="<?php echo $this->createUrl("goods",array('projectId' => $projectModel->id,'placeId' => $val->id)) ?>"><?php echo $val->name ?></a><span>10/<?php echo $goodsCount ?></span></li>
+			<li><a href="<?php echo $this->createUrl("goods",array('projectId' => $projectModel->id,'placeId' => $val->id)) ?>"><?php echo $val->name ?></a>
+                            <!--<span>10/<?php //echo $goodsCount ?></span></li>-->
 		<?php endforeach ?>		
 	</ul>				
 	<?php endif ?>
